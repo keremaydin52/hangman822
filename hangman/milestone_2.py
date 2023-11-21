6f63fdef7ecda22ab5da23c51360c16f1cc76ca1
Kerem <keremaydin52@gmail.com>
2023-11-21 10:29:41 +0000

    feat: methods added for features

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,14 +1,19 @@
 import random
 
-word_list = ['Apple', 'Banana', 'Strawberry', 'Grape', 'Melon']
-print(word_list)
+def random_fruit_picker():
+    word_list = ['Apple', 'Banana', 'Strawberry', 'Grape', 'Melon']
+    print(word_list)
 
-word = random.choice(word_list)
-print(word)
+    word = random.choice(word_list)
+    print(word)
 
-guess = input('Enter a single letter: ')
+def guess_letter():
+    guess = input('Enter a single letter: ')
 
-if len(guess) == 1 and guess.isalpha():
-    print("Good guess")
-else:
-    print("Oops! That is not a valid input.")
+    if len(guess) == 1 and guess.isalpha():
+        print("Good guess")
+    else:
+        print("Oops! That is not a valid input.")
+
+random_fruit_picker()
+guess_letter()
